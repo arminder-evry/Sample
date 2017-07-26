a65e5d68d82485c1d1b9c0becdffc8725df85761
arminderlubana <arminderlubana@users.noreply.github.com>
2017-07-26 13:25:25 +0530

    Initial Commit 3

---
+++ Model/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Labs.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string  LastName { get; set; }
+        public int Class { get; set; }
+        public int RollNumber { get; set; }
+
+    }
+}
---
+++ Model/StudentContext.cs
@@ -0,0 +1,23 @@
+﻿using Labs.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+//using Microsoft.Data.Entity;
+namespace Lab.Model
+{
+    public class StudentContext :DbContext
+    {
+        private IConfigurationRoot _config;
+        public StudentContext(IConfigurationRoot config)
+        {
+            _config = config;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_config[""]);
+           // optionsBuilder.UseSqlServer();
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
